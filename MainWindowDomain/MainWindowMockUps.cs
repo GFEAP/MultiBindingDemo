37e5543090799c09f34d57f6dc3332511de809ab
Michael Friedl <michael.friedl@gfeap.com>
2019-08-28 09:48:54 +0200

    Added alternative way of multibinding, using string format

---
+++ MainWindowDomain/MainWindowMockUps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiBindingDemo.MainWindowDomain
+{
+    public static class MainWindowMockUps
+    {
+
+        public static MainWindowViewModel MainWindowViewModelMockUp
+        {
+            get
+            {
+                return new MainWindowViewModel()
+                {
+                    Text1 = "1",
+                    Text2 = "2",
+                    Text3 = "3",
+                    Text4 = "4",
+                    ScaleFactor = 2.0d
+                };
+            }
+        }
+    }
+}
