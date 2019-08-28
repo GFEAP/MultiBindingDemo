f5ad1cf13bfef491b523dd56267c826243aceaaf
Michael Friedl <michael.friedl@gfeap.com>
2019-08-28 10:58:21 +0200

    removed unneccessary using statements

--- MainWindowDomain/MainWindowMockUps.cs
+++ MainWindowDomain/MainWindowMockUps.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MultiBindingDemo.MainWindowDomain
 {
     public static class MainWindowMockUps
     {
-
         public static MainWindowViewModel MainWindowViewModelMockUp
         {
             get
